3a9d606fec5b0fa53921bd8d6911a6c299d130cc
James Stone <jamesmstone@users.noreply.github.com>
2017-09-05 16:10:46 +1000

    get ready for submission

--- tests.sh
+++ tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-gcc -Wall -ansi -o code main.c -lm && ./code < tests/input/1.txt  > out.txt && diff out.txt tests/output/1.txt && rm out.txt code && gcc -Wall -ansi -o code main.c -lm && ./code < tests/input/0.txt  > out.txt && diff out.txt tests/output/0.txt && rm out.txt code
+gcc -Wall -ansi -o code assmt1.c -lm && ./code < tests/input/1.txt  > out.txt && diff out.txt tests/output/1.txt && rm out.txt code && gcc -Wall -ansi -o code assmt1.c -lm && ./code < tests/input/0.txt  > out.txt && diff out.txt tests/output/0.txt && rm out.txt code
